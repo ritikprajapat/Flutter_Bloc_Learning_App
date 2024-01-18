782212b505132170e689fbec9062123e7761fe52
ritikprajapat <ritikprajapat.04@gmail.com>
2024-01-18 21:23:49 +0530

    Login Bloc Logic

---
+++ lib/src/modules/auth/login/login_controller.dart
@@ -0,0 +1,17 @@
+import 'package:learning_app/src/app/app.dart';
+
+import 'bloc/login_bloc.dart';
+
+class LoginController {
+  final BuildContext context;
+  const LoginController(this.context);
+
+  void handleLogin(String type) {
+    try {
+      if (type == "email") {
+        final state = context.read<LoginBloc>().state;
+        // var emaiAddress = state.email;
+      }
+    } catch (e) {}
+  }
+}
